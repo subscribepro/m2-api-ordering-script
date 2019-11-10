2da0bc36c091766871c633e70b3b9bc034f209dc
David King <imkingdavid@gmail.com>
2019-11-10 12:25:01 -0500

    Updated script to support multiple products and setting the coupon before/after addresses

--- index.php
+++ index.php
@@ -15,49 +15,59 @@ $accessToken = '';
 $accessTokenSecret = '';
 
 $customerId = '';
-$product = [
-    'sku' => '',
-    'qty' => '',
-    'product_option' => [
-        'extension_attributes' => [
-            'subscription_option' => [ /* Allowed options */
-                'is_fulfilling' => 1,        // required
-                'option' => 'subscription',  // optional
-                'interval' => '',            // optional
-                'subscription_id' => ''      // optional
+$products = [
+    [
+        'sku' => '',
+        'qty' => '',
+        'product_option' => [
+            'extension_attributes' => [
+                'subscription_option' => [ /* Allowed options */
+                    'item_fulfils_subscription' => true,
+                    'item_added_by_subscribe_pro' => false,
+                    'interval' => '',
+                    'subscription_id' => '',
+                    'reorder_ordinal' => '',
+//                    'fixed_price' => '', // optional, but if included it cannot be empty
+                    'next_order_date' => '',
+                ],
+//                'custom_options' => [],              // optional, but if included it cannot be empty
+//                'configurable_item_options' => [],   // optional, but if included it cannot be empty
+//                'bundle_options' => []               // optional, but if included it cannot be empty
             ],
-            'custom_options' => [],              // optional, cannot be empty
-            'configurable_item_options' => [],   // optional, cannot be empty
-            'bundle_options' => []               // optional, cannot be empty
-        ]
-    ]
-];
-$addressInformation = [
-    'billing_address' => [
-        'firstname' => '',
-        'lastname' => '',
-        'company' => '',
-        'street' => ['', ''],
-        'city' => '',
-        'region_code' => '',
-        'postcode' => '',
-        'country_id' => '',
-        'telephone' => ''
+        ],
     ],
+];
+
+$billingAddressSameAsShipping = true;
+$addressInformation = [
     'shipping_address' => [
         'firstname' => '',
         'lastname' => '',
-        'company' => '',
-        'street' => ['', ''],
+        'street' => [''],
         'city' => '',
         'region_code' => '',
         'postcode' => '',
         'country_id' => '',
         'telephone' => ''
     ],
-    'shipping_method_code' => '',
-    'shipping_carrier_code' => ''
 ];
+
+$addressInformation['billing_address'] = ($billingAddressSameAsShipping === true)
+? $addressInformation['shipping_address']
+: [ // Only set these if $billingAddressSameAsShipping is false
+    'firstname' => '',
+    'lastname' => '',
+    'street' => [''],
+    'city' => '',
+    'region_code' => '',
+    'postcode' => '',
+    'country_id' => '',
+    'telephone' => ''
+];
+
+$addressInformation['shipping_carrier_code'] = '';
+$addressInformation['shipping_method_code'] = '';
+
 $payment = [
     'method' => 'subscribe_pro_vault',
     'additional_data' => [
@@ -84,7 +94,7 @@ $client = new Client([
 
 
 try {
-    /* \Magento\Quote\Api\CartManagementInterface::createEmptyCartForCustomer*/
+   /* \Magento\Quote\Api\CartManagementInterface::createEmptyCartForCustomer*/
     $response = $client->post("rest/V1/customers/{$customerId}/carts");
     $cartId = processResponse($response);
     printResult('Cart ID', $cartId);
@@ -92,19 +102,31 @@ try {
     cleanQuoteItems($client, $cartId);
 
     /* \Magento\Quote\Api\CartItemRepositoryInterface::save */
-    $response = $client->post("rest/V1/carts/{$cartId}/items", [RequestOptions::QUERY => ['cartItem' => array_merge($product, ['quote_id' => $cartId])]]);
-    $cartItems = processResponse($response);
-    printResult('Cart items', $cartItems);
+    foreach ($products as $product) {
+        $response = $client->post("rest/V1/carts/{$cartId}/items", [RequestOptions::QUERY => ['cartItem' => array_merge($product, ['quote_id' => $cartId])]]);
+        $cartItems = processResponse($response);
+        printResult('Cart items', $cartItems);
+    }
 
-    /* \Magento\Checkout\Api\ShippingInformationManagementInterface::saveAddressInformation */
-    $response = $client->post("rest/V1/carts/{$cartId}/shipping-information", [RequestOptions::QUERY => ['addressInformation' => $addressInformation]]);
-    $paymentDetails = processResponse($response);
-    printResult('Payment details', $paymentDetails);
-
+    // Remove any existing coupon, and if necessary, add one
     $response = $client->delete("rest/V1/carts/{$cartId}/coupons");
     $response = processResponse($response);
     printResult('Deleting coupon', $response);
-    if ($coupon) {
+    $couponToBeApplied = !empty($coupon);
+    if ($couponToBeApplied) {
+        $response = $client->put("rest/V1/carts/{$cartId}/coupons/{$coupon}");
+        $response = processResponse($response, true);
+        // If the coupon couldn't be applied before addresses were set, we'll try again afterwards
+        $couponToBeApplied = $response === "The coupon code isn't valid. Verify the code and try again.";
+        printResult("Applying coupon '{$coupon}'", $response);
+    }
+
+    /* \Magento\Checkout\Api\ShippingInformationManagementInterface::saveAddressInformation */
+    $response = $client->post("rest/V1/carts/{$cartId}/shipping-information", [RequestOptions::QUERY => ['addressInformation' => $addressInformation]]);
+    $addressDetails = processResponse($response);
+    printResult('Address details', $addressDetails);
+
+    if ($couponToBeApplied) {
         $response = $client->put("rest/V1/carts/{$cartId}/coupons/{$coupon}");
         $response = processResponse($response, true);
         printResult("Applying coupon '{$coupon}'", $response);
@@ -114,6 +136,8 @@ try {
     $response = $client->put("rest/V1/carts/{$cartId}/order", [RequestOptions::QUERY => ['paymentMethod' => $payment]]);
     $orderData = processResponse($response);
     printResult('Real order ID', $orderData);
+
 } catch (\Exception $e) {
-    echo $e->getMessage();
+    echo $e->getMessage() . "\n";
+    echo $e->getTraceAsString() . "\n";
 }
